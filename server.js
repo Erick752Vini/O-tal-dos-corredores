ff093d67b037012dc46e6835bc70d526e4e442eb
Luisgustavo559 <luisgustavoreistavares@gmail.com>
2026-03-02 10:17:55 -0300

    serve feito

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Servir arquivos estáticos
+app.use(express.static(__dirname));
+
+// Rota principal
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
