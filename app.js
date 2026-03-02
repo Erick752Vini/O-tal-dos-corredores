c56dafe4f0cbd608ea6079d60b781e8b65e03fe3
Luisgustavo559 <luisgustavoreistavares@gmail.com>
2026-03-02 11:14:41 -0300

    .

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-pp.use((req, res, next) => {
-    const error = new Error("Not found...");
-    error.status = 404;
-    next(error);
+app.use(express.json());
+
+app.get("/verstappen", (req, res) => {
+    res.send("Servidor funcionando")
+});
+
+app.use((req, res, next)=> {
+    res.status(404).json ({
+        erro: "Rota não encontrada"
+    })
 });
 
 module.exports = app;
